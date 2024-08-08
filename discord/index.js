c2418a8f6ea2a22831dd9b1da3a39f7bb4f82d4f
Joan Paneque Domingo <joanpd0@gmail.com>
2024-08-08 14:04:18 +0200

    New things

--- discord/index.js
+++ discord/index.js
@@ -26,8 +26,8 @@ client.on(Events.MessageCreate, async (message) => {
             // con el link enviado por el usuario y type: 'instagram'
             message.channel.send('Este link es de Instagram');
             // get the channel name
-            // axios.post(`https://yokse.joanpaneque.com/api/videos/${message.channel.name}`, {
-            axios.post(`http://localhost/api/videos/${message.channel.name}`, {
+            axios.post(`https://yokse.joanpaneque.com/api/videos/${message.channel.name}`, {
+            // axios.post(`http://localhost/api/videos/${message.channel.name}`, {
                 url: message.content,
                 type: 'instagram'
             }).then((response) => {
@@ -43,8 +43,8 @@ client.on(Events.MessageCreate, async (message) => {
             
         } else if (message.content.includes('tiktok.com')) {
             message.channel.send('Este link es de TikTok');
-            // axios.post(`https://yokse.joanpaneque.com/api/videos/${message.channel.name}`, {
-            axios.post(`http://localhost/api/videos/${message.channel.name}`, {
+            axios.post(`https://yokse.joanpaneque.com/api/videos/${message.channel.name}`, {
+            // axios.post(`http://localhost/api/videos/${message.channel.name}`, {
                 url: message.content,
                 type: 'tiktok'
             }).then((response) => {
